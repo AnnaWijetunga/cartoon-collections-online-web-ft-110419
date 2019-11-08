5bb5e6ef70e9ddfd301dfe36a46f2b128bb71ddb
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-08 14:45:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  array.collect{|x| x + .upcase}
+  array.collect{|x| x + array.upcase}
 end
 
 def long_planeteer_calls# code an argument here
