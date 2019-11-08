a1b8245059584f6a1668653c848aa369e9d052e2
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-08 15:00:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array)
   end
 end
 
-print_long_planeteer_calls(array)
+# print_long_planeteer_calls(array)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
